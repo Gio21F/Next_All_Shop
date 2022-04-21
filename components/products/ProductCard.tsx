59a77e3dfaab0149b91fe82f2ec984a4e01e602b
Giovani <giovanigonzalezchavez@gmail.com>
2022-04-21 15:53:33 -0500

    [ADD] Cart

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,6 +1,6 @@
 import { FC, useMemo, useState } from 'react';
 import NextLink from 'next/link';
-import { Grid, Card, CardActionArea, CardMedia, Box, Typography, Link } from '@mui/material'
+import { Grid, Card, CardActionArea, CardMedia, Box, Typography, Link, Chip } from '@mui/material'
 
 import { IProduct } from '../../interfaces'
 
@@ -31,6 +31,15 @@ export const ProductCard = ({ product }:Props) => {
               <NextLink href={`/product/${product.slug}`} passHref prefetch={ false }>
                 <Link>
                     <CardActionArea>
+                        {
+                            product.inStock === 0 && (
+                                <Chip 
+                                    color="primary" 
+                                    label="No hay disponibles" 
+                                    sx={{ position: 'absolute', zIndex: 1, top: 10, left: 10 }}
+                                />
+                            )
+                        }
                         <CardMedia 
                             component='img'
                             className='fadeIn'
